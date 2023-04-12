5214b44ce55ee65e6b6397844556d06967fd94c5
Raphael Merx <raphael.merx@gmail.com>
2023-04-12 20:29:03 +0800

    Change footer vars

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -10,37 +10,29 @@ export const Footer: FC = () => {
         Created by
         <a
           className="hover:opacity-50 mx-1"
-          href="https://twitter.com/mckaywrigley"
+          href="https://twitter.com/raphaelmerx"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Raphaël Merx
+        </a>
+        based on
+        <a
+          className="hover:opacity-50 ml-1"
+          href="https://github.com/mckaywrigley/wait-but-why-gpt"
           target="_blank"
           rel="noreferrer"
         >
           Mckay Wrigley
         </a>
-        based on
-        <a
-          className="hover:opacity-50 ml-1"
-          href="https://twitter.com/waitbutwhy"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Tim Urban
-        </a>
-        {`'s blog`}
-        <a
-          className="hover:opacity-50 ml-1"
-          href="https://waitbutwhy.com/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Wait But Why
-        </a>
+        {`'s work`}
         .
       </div>
 
       <div className="flex space-x-4">
         <a
           className="flex items-center hover:opacity-50"
-          href="https://twitter.com/mckaywrigley"
+          href="https://twitter.com/raphaelmerx"
           target="_blank"
           rel="noreferrer"
         >
@@ -49,7 +41,7 @@ export const Footer: FC = () => {
 
         <a
           className="flex items-center hover:opacity-50"
-          href="https://github.com/mckaywrigley/wait-but-why-gpt"
+          href="https://github.com/raphaelmerx"
           target="_blank"
           rel="noreferrer"
         >
