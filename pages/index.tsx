afac7aa02d0cc4d4d7e1c4dd563daa371fe6743f
Raphael Merx <raphael.merx@gmail.com>
2023-04-12 19:45:00 +0800

    Remove mode, api keys, navbar

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,5 @@
 import { Answer } from "@/components/Answer/Answer";
 import { Footer } from "@/components/Footer";
-import { Navbar } from "@/components/Navbar";
 import { WBWChunk } from "@/types";
 import { getImage } from "@/utils/images";
 import { IconArrowRight, IconExternalLink, IconSearch } from "@tabler/icons-react";
@@ -8,6 +7,9 @@ import endent from "endent";
 import Head from "next/head";
 import Image from "next/image";
 import { KeyboardEvent, useEffect, useRef, useState } from "react";
+
+
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
 
 export default function Home() {
   const inputRef = useRef<HTMLInputElement>(null);
@@ -17,55 +19,9 @@ export default function Home() {
   const [answer, setAnswer] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(false);
 
-  const [showSettings, setShowSettings] = useState<boolean>(false);
-  const [mode, setMode] = useState<"search" | "chat">("chat");
   const [matchCount, setMatchCount] = useState<number>(5);
-  const [apiKey, setApiKey] = useState<string>("");
-
-  const handleSearch = async () => {
-    if (!apiKey) {
-      alert("Please enter an API key.");
-      return;
-    }
-
-    if (!query) {
-      alert("Please enter a query.");
-      return;
-    }
-
-    setAnswer("");
-    setChunks([]);
-
-    setLoading(true);
-
-    const searchResponse = await fetch("/api/search", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({ query, apiKey, matches: matchCount })
-    });
-
-    if (!searchResponse.ok) {
-      setLoading(false);
-      throw new Error(searchResponse.statusText);
-    }
-
-    const results: WBWChunk[] = await searchResponse.json();
-
-    setChunks(results);
-
-    setLoading(false);
-
-    return results;
-  };
 
   const handleAnswer = async () => {
-    if (!apiKey) {
-      alert("Please enter an API key.");
-      return;
-    }
-
     if (!query) {
       alert("Please enter a query.");
       return;
@@ -81,7 +37,7 @@ export default function Home() {
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({ query, apiKey, matches: matchCount })
+      body: JSON.stringify({ query, matches: matchCount })
     });
 
     if (!searchResponse.ok) {
@@ -104,7 +60,7 @@ export default function Home() {
       headers: {
         "Content-Type": "application/json"
       },
-      body: JSON.stringify({ prompt, apiKey })
+      body: JSON.stringify({ prompt })
     });
 
     if (!answerResponse.ok) {
@@ -134,35 +90,21 @@ export default function Home() {
 
   const handleKeyDown = (e: KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter") {
-      if (mode === "search") {
-        handleSearch();
-      } else {
-        handleAnswer();
-      }
+      handleAnswer();
     }
   };
 
   const handleSave = () => {
-    if (apiKey.length !== 51) {
-      alert("Please enter a valid API key.");
-      return;
-    }
-
-    localStorage.setItem("WBW_KEY", apiKey);
     localStorage.setItem("WBW_MATCH_COUNT", matchCount.toString());
-    localStorage.setItem("WBW_MODE", mode);
 
-    setShowSettings(false);
   };
 
   const handleClear = () => {
     localStorage.removeItem("WBW_KEY");
     localStorage.removeItem("WBW_MATCH_COUNT");
-    localStorage.removeItem("WBW_MODE");
 
     setApiKey("");
     setMatchCount(5);
-    setMode("search");
   };
 
   useEffect(() => {
@@ -176,19 +118,11 @@ export default function Home() {
   useEffect(() => {
     const WBW_KEY = localStorage.getItem("WBW_KEY");
     const WBW_MATCH_COUNT = localStorage.getItem("WBW_MATCH_COUNT");
-    const WBW_MODE = localStorage.getItem("WBW_MODE");
-
-    if (WBW_KEY) {
-      setApiKey(WBW_KEY);
-    }
 
     if (WBW_MATCH_COUNT) {
       setMatchCount(parseInt(WBW_MATCH_COUNT));
     }
 
-    if (WBW_MODE) {
-      setMode(WBW_MODE as "search" | "chat");
-    }
   }, []);
 
   return (
@@ -210,78 +144,9 @@ export default function Home() {
       </Head>
 
       <div className="flex flex-col h-screen">
-        <Navbar />
         <div className="flex-1 overflow-auto">
           <div className="mx-auto flex h-full w-full max-w-[750px] flex-col items-center px-3 pt-4 sm:pt-8">
-            <button
-              className="mt-4 flex cursor-pointer items-center space-x-2 rounded-full border border-zinc-600 px-3 py-1 text-sm hover:opacity-50"
-              onClick={() => setShowSettings(!showSettings)}
-            >
-              {showSettings ? "Hide" : "Show"} Settings
-            </button>
 
-            {showSettings && (
-              <div className="w-[340px] sm:w-[400px]">
-                <div>
-                  <div>Mode</div>
-                  <select
-                    className="max-w-[400px] block w-full cursor-pointer rounded-md border border-gray-300 p-2 text-black shadow-sm focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 sm:text-sm"
-                    value={mode}
-                    onChange={(e) => setMode(e.target.value as "search" | "chat")}
-                  >
-                    <option value="search">Search</option>
-                    <option value="chat">Chat</option>
-                  </select>
-                </div>
-
-                <div className="mt-2">
-                  <div>Passage Count</div>
-                  <input
-                    type="number"
-                    min={1}
-                    max={10}
-                    value={matchCount}
-                    onChange={(e) => setMatchCount(Number(e.target.value))}
-                    className="max-w-[400px] block w-full rounded-md border border-gray-300 p-2 text-black shadow-sm focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 sm:text-sm"
-                  />
-                </div>
-
-                <div className="mt-2">
-                  <div>OpenAI API Key</div>
-                  <input
-                    type="password"
-                    placeholder="OpenAI API Key"
-                    className="max-w-[400px] block w-full rounded-md border border-gray-300 p-2 text-black shadow-sm focus:border-blue-500 focus:outline-none focus:ring-2 focus:ring-blue-500 sm:text-sm"
-                    value={apiKey}
-                    onChange={(e) => {
-                      setApiKey(e.target.value);
-
-                      if (e.target.value.length !== 51) {
-                        setShowSettings(true);
-                      }
-                    }}
-                  />
-                </div>
-
-                <div className="mt-4 flex space-x-2 justify-center">
-                  <div
-                    className="flex cursor-pointer items-center space-x-2 rounded-full bg-green-500 px-3 py-1 text-sm text-white hover:bg-green-600"
-                    onClick={handleSave}
-                  >
-                    Save
-                  </div>
-
-                  <div
-                    className="flex cursor-pointer items-center space-x-2 rounded-full bg-red-500 px-3 py-1 text-sm text-white hover:bg-red-600"
-                    onClick={handleClear}
-                  >
-                    Clear
-                  </div>
-                </div>
-              </div>
-            )}
-
-            {apiKey.length === 51 ? (
               <div className="relative w-full mt-4">
                 <IconSearch className="absolute top-3 w-10 left-1 h-6 rounded-full opacity-50 sm:left-3 sm:top-4 sm:h-8" />
 
@@ -289,7 +154,7 @@ export default function Home() {
                   ref={inputRef}
                   className="h-12 w-full rounded-full border border-zinc-600 pr-12 pl-11 focus:border-zinc-800 focus:outline-none focus:ring-1 focus:ring-zinc-800 sm:h-16 sm:py-2 sm:pr-16 sm:pl-16 sm:text-lg"
                   type="text"
-                  placeholder="What is the Instant Gratification Monkey?"
+                  placeholder="What is JSS4P?"
                   value={query}
                   onChange={(e) => setQuery(e.target.value)}
                   onKeyDown={handleKeyDown}
@@ -297,27 +162,14 @@ export default function Home() {
 
                 <button>
                   <IconArrowRight
-                    onClick={mode === "search" ? handleSearch : handleAnswer}
+                    onClick={handleAnswer}
                     className="absolute right-2 top-2.5 h-7 w-7 rounded-full bg-blue-500 p-1 hover:cursor-pointer hover:bg-blue-600 sm:right-3 sm:top-3 sm:h-10 sm:w-10 text-white"
                   />
                 </button>
               </div>
-            ) : (
-              <div className="text-center font-bold text-3xl mt-7">
-                Please enter your
-                <a
-                  className="mx-2 underline hover:opacity-50"
-                  href="https://openai.com/product"
-                >
-                  OpenAI API key
-                </a>
-                in settings.
-              </div>
-            )}
 
             {loading ? (
               <div className="mt-6 w-full">
-                {mode === "chat" && (
                   <>
                     <div className="font-bold text-2xl">Answer</div>
                     <div className="animate-pulse mt-2">
@@ -328,7 +180,6 @@ export default function Home() {
                       <div className="h-4 bg-gray-300 rounded mt-2"></div>
                     </div>
                   </>
-                )}
 
                 <div className="font-bold text-2xl mt-6">Passages</div>
                 <div className="animate-pulse mt-2">
@@ -414,7 +265,7 @@ export default function Home() {
                 ))}
               </div>
             ) : (
-              <div className="mt-6 text-center text-lg">{`AI-powered search and chat for Tim Urban's blog "Wait But Why."`}</div>
+              <div className="mt-6 text-center text-lg">{`AI-powered search and chat for DFAT reports`}</div>
             )}
           </div>
         </div>
