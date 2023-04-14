38a795f7f56da94090efa8f6c4bea12c1ee23bfd
Raphael Merx <raphael.merx@gmail.com>
2023-04-14 13:57:38 +0800

    Socials in header, hide footer on mobile

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,20 +3,27 @@ import { FC } from "react";
 
 export const Footer: FC = () => {
   return (
-    <div className="flex h-[50px] border-t border-gray-300 py-2 px-8 items-center sm:justify-between justify-center">
-      <div className="hidden sm:flex"></div>
-
-      <div className="hidden sm:flex italic text-sm">
+    <div className="flex h-[50px] border-t border-gray-300 py-2 px-8 items-center justify-center hidden sm:flex">
+      <div className="italic text-sm">
         Created by
         <a
           className="hover:opacity-50 mx-1"
-          href="https://twitter.com/raphaelmerx"
+          href="https://www.rapha.dev"
           target="_blank"
           rel="noreferrer"
         >
           Raphaël Merx
         </a>
-        based on 
+        using
+        <a
+          className="hover:opacity-50 mx-1"
+          href="https://www.dfat.gov.au"
+          target="_blank"
+          rel="noreferrer"
+        >
+          dfat.gov.au
+        </a>
+        data. Based on 
         <a
           className="hover:opacity-50 ml-1"
           href="https://github.com/mckaywrigley/wait-but-why-gpt"
@@ -28,41 +35,6 @@ export const Footer: FC = () => {
         .
       </div>
 
-      <div className="flex space-x-4">
-        <a
-          className="flex items-center hover:opacity-50"
-          href="https://twitter.com/raphaelmerx"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <IconBrandTwitter size={24} />
-        </a>
-
-        <a
-          className="flex items-center hover:opacity-50"
-          href="https://github.com/raphaelmerx"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <IconBrandGithub size={24} />
-        </a>
-        <a
-          className="flex items-center hover:opacity-50"
-          href="https://www.linkedin.com/in/raphaelmerx/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <IconBrandLinkedin size={24} />
-        </a>
-        <a
-          className="flex items-center hover:opacity-50"
-          href="https://www.rapha.dev"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <IconExternalLink size={24} />
-        </a>
-      </div>
     </div>
   );
 };
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,50 @@
-import { IconExternalLink } from "@tabler/icons-react";
+import { IconBrandGithub, IconBrandLinkedin, IconBrandTwitter, IconExternalLink } from "@tabler/icons-react";
 import Image from "next/image";
 import { FC } from "react";
 import king from "../public/king.png";
 
 export const Navbar: FC = () => {
   return (
-    <div className="flex h-[60px] border-b border-gray-300 py-2 px-4 sm:px-8 items-center justify-center">
+    <div className="flex h-[60px] border-b border-gray-300 py-2 px-4 sm:px-8 items-center sm:justify-between justify-center">
       <div className="font-semibold sm:text-2l md:text-2xl flex items-center">
         <div className="ml-2">
             AI-powered Q&A, backed by DFAT reports
         </div>
+      </div>
+      <div className="flex space-x-4 hidden sm:flex">
+        <a
+          className="flex items-center hover:opacity-50"
+          href="https://twitter.com/raphaelmerx"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <IconBrandTwitter size={24} />
+        </a>
+
+        <a
+          className="flex items-center hover:opacity-50"
+          href="https://github.com/raphaelmerx"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <IconBrandGithub size={24} />
+        </a>
+        <a
+          className="flex items-center hover:opacity-50"
+          href="https://www.linkedin.com/in/raphaelmerx/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <IconBrandLinkedin size={24} />
+        </a>
+        <a
+          className="flex items-center hover:opacity-50"
+          href="https://www.rapha.dev"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <IconExternalLink size={24} />
+        </a>
       </div>
     </div>
   );
